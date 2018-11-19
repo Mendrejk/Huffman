ba3f5a10577d76f1d68b96c7cd9060cb6d7acbf1
Mendrejk <bastian.lakomy@gmail.com>
2018-11-19 22:20:47 +0100

    Finished binary tree generation, and added individual binary code generation. Cleaned code up.

--- Huffman.py
+++ Huffman.py
@@ -17,29 +17,39 @@ def create_frequency_list(string):
     return frequency_list
 
 
-def generate_huffman_binary_tree(list):
-    if len(list) > 1:
-        a, b = list[0], list[1]
+def generate_huffman_binary_tree(freq_list):
+    while len(freq_list) > 1:
+        a, b = freq_list[0], freq_list[1]
         temp_symbol = a.symbol + b.symbol
-        temp_amount = a.count + b.count
+        temp_amount = a.amount + b.amount
         temp = Node(temp_symbol, temp_amount)
-        # removes a and b from list:
-        a = a[2:]
-        if len(list) > 0:
-            for i, node in list:
-                if temp.amount < node.amount:
-                    list.insert(temp, i)
+        temp.left, temp.right = a, b
+        # removes a and b from freq_list:
+        freq_list = freq_list[2:]
+        if len(freq_list) > 0:
+            inserted = False
+            for i, node in enumerate(freq_list):
+                if temp.amount <= node.amount:
+                    freq_list.insert(i, temp)
+                    inserted = True
                     break
+            if not inserted:
+                freq_list.append(temp)
+    return temp
 
 
 def main():
     test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi."
     frequency_list = create_frequency_list(test_string)
-    print(frequency_list)
 
     for i in range(len(frequency_list)):
         to_node = frequency_list[i]
         frequency_list[i] = Node(to_node[0], to_node[1])
+
+    tree = generate_huffman_binary_tree(frequency_list)
+    print(tree.symbol, tree.amount)
+    Node.assign_binary(tree)
+    tree.print_tree()
 
 
 if __name__ == "__main__":
--- node.py
+++ node.py
@@ -4,38 +4,23 @@ class Node:
         self.right = None
         self.symbol = symbol
         self.amount = amount
-
-    # def insert(self, symbol):
-    #     if self.symbol:
-    #         if symbol < self.symbol:
-    #             if self.left is None:
-    #                 self.left = Node(symbol)
-    #             else:
-    #                 self.left.insert(symbol)
-    #         elif symbol > self.symbol:
-    #             if self.right is None:
-    #                 self.right = Node(symbol)
-    #             else:
-    #                 self.right.insert(symbol)
-    #     else:
-    #         self.symbol = symbol
-
-    # def insert_tree(self, tree):
-    #     # assuming symbol is
-    #     if tree.symbol < self.symbol:
-    #         if self.left is None:
-    #             self.left = tree
-    #         else:
-    #             self.left.insert_tree(tree)
-    #     elif tree.symbol > self.symbol:
-    #         if self.right is None:
-    #             self.right = tree
-    #         else:
-    #             self.right.insert_tree(tree)
+    binary = None
 
     def print_tree(self):
         if self.left:
             self.left.print_tree()
-        print(self.symbol, self.amount)
+        print(self.symbol, self.amount, self.binary)
         if self.right:
             self.right.print_tree()
+
+    @staticmethod
+    def assign_binary(node):
+        if not node.binary:
+            node.binary = ''
+        if node.left:
+            node.left.binary = node.binary + '0'
+            Node.assign_binary(node.left)
+        if node.right:
+            node.right.binary = node.binary + '1'
+            Node.assign_binary(node.right)
+        return 0
